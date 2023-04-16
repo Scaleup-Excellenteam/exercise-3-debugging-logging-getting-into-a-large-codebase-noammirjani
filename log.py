0cef7ff256c92aca42dc50cb5bafe8b1a3329cbb
Noam Mirjani <Noamirjani@gmail.com>
2023-04-16 11:01:12 +0200

    fix the log code file

--- log.py
+++ log.py
@@ -1,8 +1,6 @@
 import logging
 
-
-logFile = 'logs.log'
-
+logFile = 'chess_logs.log'
 
 def get_logger():
     # Create a logger
@@ -12,7 +10,16 @@ def get_logger():
     # Create a file handler and add it to the logger
     handler = logging.FileHandler(logFile)
     handler.setLevel(logging.DEBUG)
+
+    # Create a formatter and add it to the handler
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    # Remove any existing handlers from the logger
+    for existing_handler in logger.handlers:
+        logger.removeHandler(existing_handler)
+
+    # Add the new handler to the logger
     logger.addHandler(handler)
 
     return logger
-
